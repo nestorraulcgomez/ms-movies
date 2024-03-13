7989620ed852e0f85630b25e1b66b168d281dd29
Nestor Castillo <nestorraulcgomez@gmail.com>
2024-03-12 21:26:26 -0300

    first commit

---
+++ src/main/java/com/example/msmovies/MsMoviesApplication.java
@@ -0,0 +1,13 @@
+package com.example.msmovies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsMoviesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsMoviesApplication.class, args);
+	}
+
+}
